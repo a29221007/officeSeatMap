37119ec62de9ab7c9669e1d2ee4dfe483d608dbc
jixianggong <jixianggong@longtugame.com>
2022-06-10 13:06:05 +0800

    优化移动端弹框

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 // 封装axios
 import axios from 'axios'
-// export const baseURL = 'http://maptest.longtubas.com:8091/api/' // 测试服
-export const baseURL = 'https://mapbackend.longtubas.com/api/' // 正式服
+export const baseURL = 'http://maptest.longtubas.com:8091/api/' // 测试服
+// export const baseURL = 'https://mapbackend.longtubas.com/api/' // 正式服
 // 创建axios的实例对象
 const instance = axios.create({
     baseURL,
