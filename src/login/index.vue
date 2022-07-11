eb3c80a5855be712a970cda4907a3fc746ddf584
jixianggong <jixianggong@longtugame.com>
2022-07-11 11:55:57 +0800

    判断内外网

--- src/login/index.vue
+++ src/login/index.vue
@@ -4,11 +4,36 @@
 
 <script>
 import { login } from '@/api/login.js'
+import { useRouter } from 'vue-router'
 export default {
     name:'login',
     setup(){
+        const router = useRouter()
+        function formatURL(url){
+            var searchObj = {}
+            // 找出 url 中的 ? 字符所在的位置
+            var index = url.indexOf('?')
+            if(index !== -1){
+                // 说明有参数，则格式化参数
+                var searchString = url.slice(index + 1) // 截取 ? 之后所有的字符
+                var searchArray = searchString.split('&') // 将字符串转化为数组
+                searchArray.forEach(item => {
+                    searchObj[item.split('=')[0]] = item.split('=')[1]
+                })
+            }else{
+                beginToast('fail', '链接中没有参数信息', 2000)
+                searchObj = ''
+            }
+            return searchObj
+        }
         login().then(res => {
-            window.location.href = res.data
+            let obj1 = formatURL(res.data)
+            let obj2 = formatURL(decodeURIComponent(obj1.redirect_uri + ''))
+            if(obj2.is_outer_net === '0'){
+                window.location.href = res.data
+            }else {
+                router.push('/outerNet')
+            }
         })
     }
 }
---
+++ src/outerNet.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <span>请连接公司Wi-Fi访问！</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'outerNet'
+}
+</script>
+<style lang="less" scoped>
+div{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
