8d2460ebb25337d6010fdc9cf15267475e0e3a60
jixianggong <jixianggong@longtugame.com>
2022-07-12 17:19:53 +0800

    获取会议室预约记录接口添加用户code字段

--- src/api/getMeeting.js
+++ src/api/getMeeting.js
@@ -1,6 +1,6 @@
 import request from "../utils/request"
-
+import store from '@/store'
 // 根据区域编码code获取会议室预定的历史记录
 export const getMeeting = (params) => {
-    return request('get/meeting/info','get',{"code":params})
+    return request('get/meeting/info','get',{"code":params,'userCode':store.state.UserInfo.usercode})
 }
