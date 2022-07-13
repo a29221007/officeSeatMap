52da6b9e0c1ece67d6d2368af1c80dd45873af4f
jixianggong <jixianggong@longtugame.com>
2022-07-13 14:03:46 +0800

    获取个人固资时添加加密参数

--- src/api/getFixedassets.js
+++ src/api/getFixedassets.js
@@ -1,6 +1,8 @@
 import request from "../utils/request"
+import md5 from "blueimp-md5"
 
 // 根据工号获取个人固资信息
 export const getFixedassets = ({b_usercode, v_usercode}) => {
-    return request('get/pc/getFixedassets/info','get',{b_usercode,v_usercode})
+    const time = Date.now()
+    return request('get/pc/getFixedassets/info','get',{b_usercode,v_usercode,time,token:md5(md5(v_usercode + b_usercode + time + 'bc2#6Kst7j3r2R14b='))})
 }
