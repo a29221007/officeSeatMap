00c884c247f55082e1c9ff1f4441df31636df3a9
jixianggong <jixianggong@longtugame.com>
2022-08-05 13:41:52 +0800

    测试移动端分享页面

--- src/views-rem/index.vue
+++ src/views-rem/index.vue
@@ -214,7 +214,7 @@ export default {
                     imgUrl: '/legend-image/icon_meeting.png', // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
-                        // alert('分享成功')
+                        // alert('分享成功') 
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
