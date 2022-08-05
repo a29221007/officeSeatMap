9298c76aea5e831ed010258ea48de84d53185949
jixianggong <jixianggong@longtugame.com>
2022-08-05 13:50:25 +0800

    测试移动端分享

--- src/views-rem/index.vue
+++ src/views-rem/index.vue
@@ -215,7 +215,7 @@ export default {
                     imgUrl: image, // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
-                        // alert('分享成功') 
+                        // alert('分享成功')
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
