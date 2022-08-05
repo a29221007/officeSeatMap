bb5977f1fd819d170334b11c70b8ed44c0325a40
jixianggong <jixianggong@longtugame.com>
2022-08-05 14:32:25 +0800

    测试移动端分享框图片

--- src/views-rem/index.vue
+++ src/views-rem/index.vue
@@ -215,7 +215,7 @@ export default {
                     imgUrl: 'https://photo.16pic.com/00/45/79/16pic_4579388_b.jpg', // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
-                        // alert('分享成功')
+                        // alert('分享成功') 
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
