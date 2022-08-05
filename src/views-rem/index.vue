9d90ff163e8ed5b68bb67bcec29594b03a4b62b6
jixianggong <jixianggong@longtugame.com>
2022-08-05 14:26:34 +0800

    测试移动端分享框图片

--- src/views-rem/index.vue
+++ src/views-rem/index.vue
@@ -212,7 +212,7 @@ export default {
                     title: '测试标题', // 分享标题
                     desc: '测试描述', // 分享描述
                     link: 'http://maptest.longtubas.com/home', // 分享链接；在微信上分享时，该链接的域名必须与企业某个应用的可信域名一致
-                    imgUrl: image, // 分享图标
+                    imgUrl: 'https://photo.16pic.com/00/45/79/16pic_4579388_b.jpg', // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
                         // alert('分享成功')
